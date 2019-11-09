54fc017cf4e2c1323e3a30c14e1a7cfb875af24f
Peng Bin <BinPeng@usst.edu.cn>
2019-11-09 17:55:42 +0800

    10

---
+++ build/CMakeFiles/mpi_stubs.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "styles/gitversion.h"
+  "CMakeFiles/mpi_stubs.dir/mnt/c/users/Bin_Peng/source/repos/lammps/src/STUBS/mpi.c.o"
+  "libmpi_stubs.pdb"
+  "libmpi_stubs.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mpi_stubs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
